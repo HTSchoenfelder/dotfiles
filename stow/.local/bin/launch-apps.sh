3f5a0242f7654c52d827372b060aec522face448
Henrik Schönfelder <mail@henrikschoenfelder.de>
2024-03-29 11:44:16 +0100

    updated launch-apps.sh

--- stow/.local/bin/launch-apps.sh
+++ stow/.local/bin/launch-apps.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-. /home/henrik/files/launch-apps.sh
+nohup google-chrome --profile-directory="Default" &> /dev/null &
+disown
+
+nohup google-chrome --profile-directory="Profile 1" &> /dev/null &
+disown
+
+nohup google-chrome --profile-directory="Profile 2" &> /dev/null &
+disown
+
+nohup google-chrome --profile-directory="Profile 3" &> /dev/null &
+disown
+
+code /home/henrik/terminal
+code /home/henrik/files
+code /home/henrik/dotfiles
+
+. /home/henrik/files/launch-projects.sh
