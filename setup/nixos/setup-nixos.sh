182dc2c599e1fdc78c6bcbd15d387af690e4e713
Henrik Schönfelder <mail@henrikschoenfelder.de>
2024-09-26 16:00:56 +0200

    update

--- setup/nixos/setup-nixos.sh
+++ setup/nixos/setup-nixos.sh
@@ -1,3 +1,7 @@
 #! /usr/bin/env bash
 
 sudo mv /etc/nixos /etc/nixos.bak
+
+nix --extra-experimental-features 'nix-command flakes' flake show
+
+sudo NIXOS_EXTRA_EXPERIMENTAL_FEATURES="nix-command flakes" nixos-rebuild switch --flake .
