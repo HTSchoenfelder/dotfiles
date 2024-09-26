7805dc3f525f2820b10b1be0f8cc45a4169c0ef0
Henrik Schönfelder <mail@henrikschoenfelder.de>
2024-09-26 16:14:08 +0200

    update

--- setup/nixos/setup-nixos.sh
+++ setup/nixos/setup-nixos.sh
@@ -1,7 +1,23 @@
 #! /usr/bin/env bash
 
+# nix-shell -p git
+
+echo "Moving /etc/nixos to /etc/nixos.bak..."
+read -p "(Press Enter to continue)" enter
+
 sudo mv /etc/nixos /etc/nixos.bak
+echo "Moved /etc/nixos to /etc/nixos.bak"
+
+if ! command -v git &> /dev/null
+then
+    echo "Git is not available."
+    exit 1
+fi
+echo "Git is available, continuing..."
 
 nix --extra-experimental-features 'nix-command flakes' flake show
+
+echo "Applying nixos configuration..."
+read -p "(Press Enter to continue)" enter
 
 sudo NIXOS_EXTRA_EXPERIMENTAL_FEATURES="nix-command flakes" nixos-rebuild switch --flake .
