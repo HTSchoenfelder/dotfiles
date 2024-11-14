9984cf6de2a332e16ef00ce98c646437f3c1ee75
Henrik Schönfelder <mail@henrikschoenfelder.de>
2024-11-14 11:59:46 +0100

    update

--- setup/nixos/flake.nix
+++ setup/nixos/flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     nixpkgs.url = "git+https://github.com/NixOS/nixpkgs?ref=nixos-unstable";
     nixpkgs-stable.url = "git+https://github.com/NixOS/nixpkgs?ref=nixos-24.05";
-    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
+    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1&ref=v0.45.0";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
