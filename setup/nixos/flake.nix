49ac87d8734a8524461d30a487e1ca9022b0c5c0
Henrik Schönfelder <mail@henrikschoenfelder.de>
2024-11-15 12:52:48 +0100

    update

--- setup/nixos/flake.nix
+++ setup/nixos/flake.nix
@@ -2,41 +2,32 @@
   inputs = {
     nixpkgs.url = "git+https://github.com/NixOS/nixpkgs?ref=nixos-unstable";
     nixpkgs-stable.url = "git+https://github.com/NixOS/nixpkgs?ref=nixos-24.05";
-    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1&ref=v0.45.0";
+    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      nixpkgs-stable,
-      home-manager,
-      ...
-    }@inputs:
-    let
-      stablePkgs = import nixpkgs-stable {
-        system = "x86_64-linux";
-        config = {
-          allowUnfree = true;
-        };
-      };
-    in
-    {
-      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-        specialArgs = {
-          inherit inputs stablePkgs;
-        };
-        system = "x86_64-linux";
-        modules = [
-          ./configuration.nix
-          ./hardware-desktop.nix
-          ./packages.nix
-          home-manager.nixosModules.home-manager
-        ];
+  outputs = { self, nixpkgs, nixpkgs-stable, home-manager, ... }@inputs: let
+    stablePkgs = import nixpkgs-stable {
+      system = "x86_64-linux";
+      config = {
+        allowUnfree = true;
       };
     };
+  in {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      specialArgs = {
+        inherit inputs stablePkgs;
+      };
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        ./hardware-desktop.nix
+        ./packages.nix
+        home-manager.nixosModules.home-manager
+      ];
+    };
+  };
 }
