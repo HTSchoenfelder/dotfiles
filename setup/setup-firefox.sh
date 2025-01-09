e9a8d9ce3fd5ba7ee896e0faf568095b7663144a
Henrik Schönfelder <mail@henrikschoenfelder.de>
2025-01-09 18:18:51 +0100

    cleanup

--- setup/setup-firefox.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env bash
-
-pkill -x firefox
-firefox --createprofile "defaultprofile $HOME/.mozilla/firefox/defaultprofile"
-firefox --no-remote -P "defaultprofile" --new-instance "about:profiles"
