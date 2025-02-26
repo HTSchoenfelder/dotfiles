5663e517bcf649ed17f13bb2bfc7ba76591f8bbb
Henrik Schönfelder <mail@henrikschoenfelder.de>
2025-02-26 11:04:57 +0100

    update

--- home/.config/hypr/scripts/launch-chrome-instances.sh
+++ home/.config/hypr/scripts/launch-chrome-instances.sh
@@ -1,12 +1,12 @@
 #! /usr/bin/env bash
 
-./launch-chrome.sh "Default"
+$HOME/.config/hypr/scripts/launch-chrome.sh "Default"
 sleep 1
 
-./launch-chrome.sh "Profile 1"
+$HOME/.config/hypr/scripts/launch-chrome.sh "Profile 1"
 sleep 1
 
-./launch-chrome.sh "Profile 2"
+$HOME/.config/hypr/scripts/launch-chrome.sh "Profile 2"
 sleep 1
 
-./launch-chrome.sh "Profile 3"
+$HOME/.config/hypr/scripts/launch-chrome.sh "Profile 3"
