5ad7493a8bdd1e688b35e676e127b9f3033bce50
Henrik Schönfelder <mail@henrikschoenfelder.de>
2025-02-27 14:36:53 +0100

    update

--- home/.config/hypr/scripts/notify-player-current-track.sh
+++ home/.config/hypr/scripts/notify-player-current-track.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env bash
 
-title=$(playerctl -p spotify metadata title)
-artist=$(playerctl  -p spotify metadata artist)
-iconUrl=$(playerctl  -p spotify metadata mpris:artUrl)
+title=$(playerctl -p spotify_player metadata title)
+artist=$(playerctl  -p spotify_player metadata artist)
+iconUrl=$(playerctl  -p spotify_player metadata mpris:artUrl)
 curl -o /tmp/currenttrack $iconUrl
 
 notify-send -t 2000 -i /tmp/currenttrack "${title}" "${artist}"
