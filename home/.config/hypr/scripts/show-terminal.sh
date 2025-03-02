fca32dbfe6be199e500ebda1915fa9692a855013
Henrik Schönfelder <mail@henrikschoenfelder.de>
2025-03-02 18:16:20 +0100

    update

--- home/.config/hypr/scripts/show-terminal.sh
+++ home/.config/hypr/scripts/show-terminal.sh
@@ -1,24 +1,32 @@
 #! /usr/bin/env bash
 
-if [ "$#" -eq 1 ]; then
-    COMMAND=$1
-else
-    hyprctl notify -1 5000 "rgb(ff1ea3)" "Usage: $0 <monitor>"
+if [ "$#" -ne 1 ]; then
+    hyprctl notify -1 5000 "rgb(ff1ea3)" "Usage: $0 <command>"
     exit 1
 fi
 
-CLASS=$(hyprctl activewindow -j | jq -r '.class')
+COMMAND=$1
+ACTIVE_WINDOW_INFO=$(hyprctl activewindow -j)
+CLASS=$(echo "$ACTIVE_WINDOW_INFO" | jq -r '.class')
+TITLE=$(echo "$ACTIVE_WINDOW_INFO" | jq -r '.title')
+PROJECT=$(echo "$TITLE" | sed 's/ | Code//')
 
 if [[ $CLASS == "code" ]]; then
-    PROJECT=$(hyprctl activewindow -j | jq -r '.title' | sed 's/ | Code//')
-    TERMINAL_EXISTS=$(hyprctl clients -j | jq -r '.[].class' | grep "code-terminal-$PROJECT-$COMMAND")
-    if [[ -z $TERMINAL_EXISTS ]]; then
-        hyprctl dispatch exec "kitty --class code-terminal-$PROJECT-$COMMAND --title \"$PROJECT | Terminal\" --working-directory $PROJECT $COMMAND"
-    else
-        hyprctl dispatch focuswindow "class:code-terminal-$PROJECT-$COMMAND"
-    fi
-elif [[ $CLASS == "code-terminal-"* ]]; then
+    TARGET_CLASS="code-terminal-$PROJECT-$COMMAND"
+elif [[ $CLASS == code-terminal-* ]]; then
+    PROJECT=$(echo "$CLASS" | sed 's/code-terminal-\(.*\)-.*/\1/')
+    TARGET_CLASS="code-terminal-$PROJECT-$COMMAND"
+else
+    hyprctl notify 0 1000 0 "Active window is not a VS Code window or associated terminal"
+    exit 1
+fi
+
+EXISTING_TERMINAL=$(hyprctl clients -j | jq -r '.[].class' | grep "$TARGET_CLASS")
+
+if [[ -z $EXISTING_TERMINAL ]]; then
+    hyprctl dispatch exec "kitty --class $TARGET_CLASS --title \"$PROJECT | Terminal\" --working-directory $PROJECT $COMMAND"
+elif [[ $CLASS == "$TARGET_CLASS" ]]; then
     hyprctl dispatch togglespecialworkspace code-terminal
 else
-    hyprctl notify 0 1000 0 "Active window is not a VS Code window"
+    hyprctl dispatch focuswindow "class:$TARGET_CLASS"
 fi
