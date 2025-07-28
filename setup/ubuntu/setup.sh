8de4dbe74c1128db07d00a7d3d1f96df799803db
Henrik Schönfelder <mail@henrikschoenfelder.de>
2025-07-28 13:20:26 +0200

    update

--- setup/ubuntu/setup.sh
+++ setup/ubuntu/setup.sh
@@ -4,6 +4,10 @@ sudo apt update && sudo apt install -y \
     stow \
     jq \
     bat \
-    zsh 
+    zsh \
+    fzf \
+    kubectl \
+    direnv \
+    zoxide
 
 curl -sS https://starship.rs/install.sh | sh
