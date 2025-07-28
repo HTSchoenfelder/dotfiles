f895af200e292800737e0ca8da601bcd088ab09c
Henrik Schönfelder <mail@henrikschoenfelder.de>
2025-07-28 13:27:10 +0200

    update

--- setup/ubuntu/setup.sh
+++ setup/ubuntu/setup.sh
@@ -14,7 +14,7 @@ curl -sS https://starship.rs/install.sh | sh
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
-ZSH_CUSTOM_DEFAULT=/usr/share/zsh/plugins
+ZSH_CUSTOM_DEFAULT=/usr/share/zsh
 git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-$ZSH_CUSTOM_DEFAULT}/plugins/zsh-autosuggestions
 git clone --depth 1 https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$ZSH_CUSTOM_DEFAULT}/plugins/fast-syntax-highlighting
 
