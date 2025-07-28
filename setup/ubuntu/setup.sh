ae1f243d12a32f20aff6a4e8c069f6d8f3bf9d11
Henrik Schönfelder <mail@henrikschoenfelder.de>
2025-07-28 13:46:17 +0200

    update

--- setup/ubuntu/setup.sh
+++ setup/ubuntu/setup.sh
@@ -7,7 +7,8 @@ sudo apt update && sudo apt install -y \
     zsh \
     fzf \
     direnv \
-    zoxide
+    zoxide \
+    wget
 
 curl -sS https://starship.rs/install.sh | sh
 
@@ -19,3 +20,5 @@ git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_C
 git clone --depth 1 https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$ZSH_CUSTOM_DEFAULT}/plugins/fast-syntax-highlighting
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg && sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/ && sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list' && rm -f packages.microsoft.gpg && sudo apt-get install apt-transport-https && sudo apt-get update && sudo apt-get install -y code
