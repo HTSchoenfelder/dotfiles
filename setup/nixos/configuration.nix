87bc37db38e264daf3c3471d6bf284c858c705b3
Henrik Schönfelder <36896808+HTSchoenfelder@users.noreply.github.com>
2026-09-19 16:21:13 +0200

    start Hyprland through start-hyprland

--- setup/nixos/configuration.nix
+++ setup/nixos/configuration.nix
@@ -75,7 +75,7 @@ in
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --asterisks --cmd Hyprland";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --asterisks --cmd ${hyprlandPackage}/bin/start-hyprland";
         user = "${userName}";
       };
     };
